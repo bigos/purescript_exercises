40592d77d663d39d9d7303c9fe50e2e0fd08c5e1
Jacek Podkanski <ruby.object@googlemail.com>
2025-07-09 20:36:11 +0100

    code for flip

--- src/Ch5.purs
+++ src/Ch5.purs
@@ -1,8 +1,19 @@
 module Ch5 where
 
-import Prelude (Unit)
+import Prelude (Unit, show, const)
 import Effect (Effect)
 import Effect.Console (log)
 
+-- the last parentheses are implied
+-- these type signatures are equivalent
+flip :: ∀ a b c. (a -> b -> c) -> (b -> a -> c)
+-- flip :: ∀ a b c. (a -> b -> c) -> b -> (a -> c)
+-- flip :: ∀ a b c. (a -> b -> c) -> b -> a -> c
+--
+-- flip f x y = f y x
+-- flip f = \x y -> f y x
+flip f x = \y  -> f y x
+--  the above is example of alternative syntaxes with moving parameters avross te =
+
 test :: Effect Unit
-test =  log "test"
+test =  log (show (flip const 1 2))
