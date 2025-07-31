e6cf56a832da9357cde8f613bb5f7500d87bc86c
Jacek Podkanski <ruby.object@googlemail.com>
2025-07-31 19:55:06 +0100

    showing

--- src/Ch7a.purs
+++ src/Ch7a.purs
@@ -1,11 +1,11 @@
 module Ch7a where
 
-import Prelude (Unit, show, discard, (==), ($), (<), (>), (<=), (>=))
-
 import Data.Eq (class Eq)
+import Data.Ord (class Ord, Ordering(..), compare)
 import Effect (Effect)
 import Effect.Console (log)
-import Data.Ord (class Ord, Ordering(..), compare)
+import Prelude (Unit, show, discard, (==), ($), (<), (>), (<=), (>=), (<>), (||))
+import Data.Show (class Show)
 
 data Maybe a = Nothing | Just a
 
@@ -13,6 +13,17 @@ instance eqMaybe :: Eq a => Eq (Maybe a) where
   eq Nothing Nothing = true
   eq (Just x) (Just y) = x == y
   eq _ _ = false
+
+instance ordMaybe :: Ord a => Ord (Maybe a) where
+  compare Nothing Nothing = EQ
+  compare (Just x) (Just y) = compare x y
+  compare Nothing _ = LT
+  compare _ Nothing = GT
+
+-- add Show a to make  Show (Maybe a) showable
+instance showMaybe :: Show a => Show (Maybe a) where
+  show Nothing = "Nothing"
+  show (Just x) = "(Just " <> show x <> ")"
 
 test :: Effect Unit
 test = do
@@ -28,3 +39,5 @@ test = do
   log $ show $ Just 10 >= Just 10
   log $ show $ Just 99 > Nothing
   log $ show $ Just 99 < Nothing
+  log $ show $ Just "abc"
+  log $ show $ (Nothing :: Maybe Unit)
